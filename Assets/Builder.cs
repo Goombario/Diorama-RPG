87d5fbf89e3e8c88160dd729e0104cdd78faa463
Graham <graham.watson@ontariotechu.net>
2020-03-31 22:26:34 -0400

    Show build preview

---
+++ Assets/Builder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    [SerializeField] private Transform previewCube = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        previewCube.GetComponent<MeshRenderer>().enabled = false;
+        ShowPreview();
+    }
+
+    void ShowPreview()
+    {
+        // Raycast tutorial: https://www.youtube.com/watch?v=_yf5vzZ2sYE
+
+        var cast = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(cast, out hit))
+        {
+            var selection = hit.transform;
+            var center = selection.GetComponent<Renderer>().bounds.center;
+            previewCube.SetPositionAndRotation(center + hit.normal, Quaternion.identity);
+            previewCube.GetComponent<MeshRenderer>().enabled = true;
+            
+        }
+    }
+}
